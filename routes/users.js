4cfc6ec5ef0bbe68177e58392e752c1ee58e7a9a
Hanna <igerne94@gmail.com>
2023-01-15 13:20:31 +0100

    handling registration form

--- routes/users.js
+++ routes/users.js
@@ -18,4 +18,67 @@ router.get('/login', function(req, res, next) {
   });
 });
 
+router.post('/register', function(req, res, next){
+	// Get Form Values
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	// Check for Image Field
+	if (req.files.profileimage) {
+		console.log('Uploading File...');
+
+		// File Info
+		var profileImageOriginalName = req.files.profileimage.originalname;
+		var profileImageName = req.files.profileimage.name;
+		var profileImageMime = req.files.profileimage.mimetype;
+		var profileImagePath = req.files.profileimage.path;
+		var profileImageExt = req.files.profileimage.extension;
+		var profileImageSize = req.files.profileimage.size;
+	} else {
+		// Set a Default Image
+		var profileImageName = 'noimage.png';
+	}
+
+	// Form Validation
+	req.checkBody('name','Name field is required').notEmpty();
+	req.checkBody('email','Email field is required').notEmpty();
+	req.checkBody('email','Email not valid').isEmail();
+	req.checkBody('username','Username field is required').notEmpty();
+	req.checkBody('password','Password field is required').notEmpty();
+	req.checkBody('password2','Passwords do not match').equals(req.body.password);
+
+	// Check for errors
+  // ...And setting up them visible in register.jade
+	var errors = req.validationErrors();
+
+	if (errors) {
+		res.render('register', {
+			errors: errors,
+			name: name,
+			email: email,
+			username: username,
+			password: password,
+			password2: password2
+		});
+	} else {
+		var newUser = new User({ //TODO: create User module
+			name: name,
+			email: email,
+			username: username,
+			password: password,
+			profileimage: profileImageName
+		});
+
+		// Success Message by flash
+		req.flash('success','You are now registered and may log in');
+
+		res.location('/');
+		res.redirect('/');
+	}
+
+});
+
 module.exports = router;
